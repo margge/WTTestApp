20ef00d1c95bdfbc95e9100fbba6b7fe73c3b105
margge <diana.guiza@periferia-it.com>
2015-06-20 10:06:05 -0500

    Initial Commit

---
+++ wt_login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wt_login
+//
+//  Created by Margge Guiza on 6/20/15.
+//  Copyright (c) 2015 Margge Guiza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
